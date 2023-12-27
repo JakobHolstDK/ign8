443eb33aa78854d47ba50b37b85d9f7521fa0a83
Jakob Holst <jho@miracle.dk>
2023-12-27 20:32:06 +0100

    requirement

--- src/ign8/common.py
+++ src/ign8/common.py
@@ -1,5 +1,7 @@
 import os
 import datetime
+import subprocess 
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -27,6 +29,11 @@ def line_in_file(file_path, search_text):
             if search_text in line:
                 return True
     return False
+def runme(command):
+  commandlist = command.split(" ")
+  result = subprocess.run(commandlist, capture_output=True)
+  payload = {"returncode": result.returncode, "stdout": result.stdout.decode(), "stderr": result.stderr }
+  return payload
 
     
 
@@ -37,6 +44,19 @@ def get_file_content(file_path):
 def get_file_content_lines(file_path):
     with open(file_path, 'r') as f:
         return f.readlines()
+    
+# 127.0.0.1 user-identifier frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326
+    #    127.0.0.1 is the IP address of the client (remote host) which made the request to the server.
+    # user-identifier is the RFC 1413 identity of the client. Usually "-".
+    #frank is the userid of the person requesting the document. Usually "-" unless .htaccess has requested authentication.
+    #[10/Oct/2000:13:55:36 -0700] is the date, time, and time zone that the request was received, by default in strftime format %d/%b/%Y:%H:%M:%S %z.
+    #"GET /apache_pb.gif HTTP/1.0" is the request line from the client. The method GET, /apache_pb.gif the resource requested, and HTTP/1.0 the HTTP protocol.
+    #200 is the HTTP status code returned to the client. 2xx is a successful response, 3xx a redirection, 4xx a client error, and 5xx a server error.
+    #2326 is the size of the object returned to the client, measured in bytes.
+# we need to rewrite prettylog to use this format
+    
+
+    
 
 def prettyllog(function, action, item, organization, statuscode, text, severity="INFO"):
   silence = False
