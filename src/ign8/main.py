b4ca091840eb7093de9f22fb7c8e6f44692d3513
Jakob Holst <jho@miracle.dk>
2024-01-03 10:59:53 +0100

    dmo

--- src/ign8/main.py
+++ src/ign8/main.py
@@ -8,9 +8,10 @@ def main():
 
 def serve():
     prettyllog("main", "check", "main", "all", "200", "Success")
-    os.system("pip install --upgrade ign8")
+    os.system("pip install --upgrade ign8 >/dev/null 2>&1")
     os.chdir("/usr/local/lib/python3.9/site-packages/ign8/ui/project/ignite/")
-    gunicorn = subprocess.Popen(["gunicorn", "ignite.wsgi", "-c", "gunicorn.conf"])
+    #p = Popen(['espeak', '-b', '1'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+    gunicorn = subprocess.Popen(["gunicorn", "ignite.wsgi", "-c", "gunicorn.conf"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     prettyllog("main", "check", "main", "all", "200", "Success", "info")
     gunicorn.wait()
 
