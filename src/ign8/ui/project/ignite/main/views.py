df23812be4d90ac1232bfe39c181f277fac5f2a5
Jakob Holst <jho@miracle.dk>
2024-01-03 13:08:36 +0100

    add main2

--- src/ign8/ui/project/ignite/main/views.py
+++ src/ign8/ui/project/ignite/main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from .models import maindata
+from .models import maindata, services
+
 from rest_framework import viewsets
 
 
 def mainview(request):
     maindata_entries = maindata.objects.all()
-    return render(request, 'main.html', {'selinux_entries': maindata_entries})
+    maindata_services = services.objects.all()
+    return render(request, 'main.html', {'maindata_entries': maindata_entries, 'title': 'IGN8', 'maindata_services': maindata_services})
