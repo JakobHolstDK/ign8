5ca32af42841d6f8abf5fd33f8fc593f3f201117
Jakob Holst <jho@miracle.dk>
2024-01-03 08:30:22 +0100

    dmo

--- src/ign8/main.py
+++ src/ign8/main.py
@@ -1,5 +1,6 @@
 from .common import prettyllog
 import os
+import subprocess
 
 def main():
     prettyllog("main", "check", "main", "all", "200", "Success")
@@ -8,7 +9,14 @@ def main():
 def serve():
     prettyllog("main", "check", "main", "all", "200", "Success")
     os.command("pip install --uprade ign8")
-    os.command("cd /"
+    os.chdir("/usr/local/lib/python3.9/site-packages/ign8/ui/project/ignite/")
+    gunicorn = subprocess.Popen(["gunicorn", "ignite.wsgi", "-c", "gunicorn.conf"])
+    prettyllog("main", "check", "main", "all", "200", "Success", "info")
+    gunicorn.wait()
+
+
+
+#    os.command("gunicorn ignite.wsgi -c gunicorn.conf")
     # change working directory to the root of the project
 
 
