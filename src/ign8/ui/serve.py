c87065f0328e6e889300ef971834ceefcc37a413
Jakob Holst <rks221@ku.it.dk>
2024-01-09 14:03:09 +0100

    nginx.conf dest to tmp

--- src/ign8/ui/serve.py
+++ src/ign8/ui/serve.py
@@ -13,6 +13,18 @@ def main():
     ign8_ui_host = os.environ.get("IGN8_UI_HOST", "ign8.openknowit.com")
     ign8_ui_debug = os.environ.get("IGN8_UI_DEBUG", "True")
 
+    # change to the ui directory
+    VIR_ENV = os.environ.get("VIRTUAL_ENV", "/opt/ign8")
+    os.chdir(VIR_ENV/ + "/lib/python3.9/site-packages/ign8/ui/project/ignite")
+
+    # run the server
+    myserver = subprocess.run(["gunicon", "--bind", ign8_ui_host + ":" + ign8_ui_port, "--workers", "3", "ignite.wsgi -c gunicorn.conf.py --log-level=debug"])
+    myserver.wait()
+    return 0
+
+
+
+
 
     
 
