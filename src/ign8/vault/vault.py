50a8bcd34751cc080b1b39c3145c1793194ab56f
Jakob Holst <rks221@ku.it.dk>
2024-01-16 10:08:07 +0100

    vault errror hanfling

--- src/ign8/vault/vault.py
+++ src/ign8/vault/vault.py
@@ -1,24 +1,26 @@
 import os
 import requests
 import pprint
+from ..common import prettyllog
+
 
 
 def getenv(myenv):
-    myenv['VAULT_URL'] = os.environ.get("IGN_VAULT_URL", None)
-    myenv['VAULT_TOKEN'] = os.environ.get("IGN_VAULT_TOKEN", None)
+    myenv['VAULT_URL'] = os.environ.get("IGN8_VAULT_URL", None)
+    myenv['VAULT_TOKEN'] = os.environ.get("IGN8_VAULT_TOKEN", None)
     return myenv
 
 
 
 def checkaccess():
     myenv = getenv({})
-    pprint.pprint(myenv)
     if myenv['VAULT_URL'] is None:
-        print("No vault URL set")
+        prettyllog("vault", "checkaccess", myenv['VAULT_URL'], "-", "000", "Environment not set vault url", "ERROR")
         return None
     if myenv['VAULT_TOKEN'] is None:
-        print("No vault token set")
+        prettyllog("vault", "checkaccess", myenv['VAULT_URL'], "-", "000", "Environment not set vault token", "ERROR")
         return None
+    
     headers = {'X-Vault-Token': myenv['VAULT_TOKEN']}   
     checkaccess = requests.get(myenv['VAULT_URL']+"/api/v1/healthcheck/", headers=headers, verify=False)
     if checkaccess.status_code == 200:
@@ -29,9 +31,13 @@ def checkaccess():
 
 
 def getlogin(path):
-    checkaccess()
-    vaultlogin = requests.get("http://localhost:8000/api/v1/vault/login/?path=" + path)
-    if vaultlogin.status_code == 200:
-        return vaultlogin.json()
+    if checkaccess():
+
+        vaultlogin = requests.get("http://localhost:8000/api/v1/vault/login/?path=" + path)
+        if vaultlogin.status_code == 200:
+            return vaultlogin.json()
+        else:
+            return None
     else:
         return None
+    
