b09ccd5952bcc64f77874f7882b83def9059a69b
Jakob Holst <rks221@ku.it.dk>
2024-01-16 09:40:15 +0100

    doman logn automate

--- src/ign8/podman/__init__.py
+++ src/ign8/podman/__init__.py
@@ -1,3 +1,9 @@
 from .serve import serve
 from .podman import getenv
-from .vault import getenv
+
+def main():
+    myenv = {}
+    myenv = getenv(myenv)
+    print(myenv)
+    print("Serving")
+    serve()
