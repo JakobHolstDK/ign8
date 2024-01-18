70e237345286b5c6f75bae3acbdf9622b42dc2c7
jakob holst <jakob.holst@knowit.dk>
2024-01-18 10:47:06 +0100

    include rest franmeworK

--- src/ign8/ui/project/ignite/ignite/settings.py
+++ src/ign8/ui/project/ignite/ignite/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',  # Add this line for Django REST framework
     'main',
     'selinux',
 ]
