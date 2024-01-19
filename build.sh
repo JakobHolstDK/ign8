7aee6a7b0348b85f621ed5606c2ac6ddd8fcf002
Jakob Holst <jho@miracle.dk>
2024-01-19 14:49:27 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   build.sh
    	modified:   pyproject.toml
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    	dist/ign8-4.4.22-py3-none-any.whl
    	dist/ign8-4.4.22.tar.gz
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@ ign8_bump patch
 MYMESSAGE=$(git status)
 git commit -a -m  "$MYMESSAGE"
 git push 
-curl -X POST ${IGN8_JENKINS_URL}/job/ign8/build \
-  --user jho:${IGN8_JENKINS_TOKEN}
+rm -r dist || echo
+poetry update
+poetry build
+poetry run twine upload dist/* || echo
+#curl -X POST ${IGN8_JENKINS_URL}/job/ign8/build \
+#  --user jho:${IGN8_JENKINS_TOKEN}
 
