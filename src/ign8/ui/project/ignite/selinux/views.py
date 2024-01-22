40a9109b28e033bd153704577b9018ad6c0f0fbc
Jakob Holst <jakob.holst@knowit.dk>
2024-01-22 11:05:36 +0100

    correct default api

--- src/ign8/ui/project/ignite/selinux/views.py
+++ src/ign8/ui/project/ignite/selinux/views.py
@@ -157,7 +157,6 @@ def host_message(request, pk=None):
     # Get the message for the specified host
     host_message = get_object_or_404(message, digest=pk)
     suggestions = suggestion.objects.filter(messagedigest=pk)
-    print(suggestions)
 
     context = {
         'host_message': host_message,
