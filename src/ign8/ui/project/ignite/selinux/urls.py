d6375bf49e237c2dc05e36b35e84c5981537e6cf
Jakob Holst <jakob.holst@knowit.dk>
2024-01-26 14:20:38 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   pyproject.toml
    	modified:   src/ign8/ui/project/ignite/selinux/urls.py
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- src/ign8/ui/project/ignite/selinux/urls.py
+++ src/ign8/ui/project/ignite/selinux/urls.py
@@ -3,13 +3,9 @@ from django.urls import path, include
 from .views import selinux_list, selinux_event_list , UploadSelinuxDataView, UploadSElinuxEventView, message_list
 from .views import SetroubleshootEntry_list, SetroubleshootEntry_host, SetroubleshootEntryAPIview, messageAPIview, suggestionAPIview
 from .views import selinuxAPIview, SetroubleshootEntry_list_full, host_message
-
 from rest_framework.routers import DefaultRouter
 
-
-
 urlpatterns = [
-    urlpatterns = [
     path('', selinux_list, name='selinux_list'),
     path('messages/', message_list, name='selinux_messages_list'),
     path('messages/<pk>', message_list, name='selinux_messages_by_host_list'),
