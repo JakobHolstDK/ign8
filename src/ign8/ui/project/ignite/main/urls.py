e104a3b2cab9e64fd6d46712357182421249a93f
Jakob Holst <jakob.holst@knowit.dk>
2024-01-26 14:46:27 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   pyproject.toml
    	modified:   src/ign8/ui/project/ignite/main/urls.py
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- src/ign8/ui/project/ignite/main/urls.py
+++ src/ign8/ui/project/ignite/main/urls.py
@@ -7,6 +7,7 @@ from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('', mainview, name='ignite_home'),
-    path('project/<name>', projectdetail,  name='project_detail'),
+    path('project/', projectdetail,  name='project_detail'),
+    path('project/<name>', projectdetail,  name='project_detail_by_name'),
 
 ]
