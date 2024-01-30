6294e09602269be2891026bbf4b344b4b2fa5fcf
jakob holst <jakob.holst@knowit.dk>
2024-01-30 12:26:32 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   pyproject.toml
    	modified:   src/ign8/ui/project/ignite/ignite/urls.py
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- src/ign8/ui/project/ignite/ignite/urls.py
+++ src/ign8/ui/project/ignite/ignite/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-      path('selinux/', include('selinux.urls')),
+    path('admin/', admin.site.urls),
+    path('', include("main.urls")),
 ]
 
 
