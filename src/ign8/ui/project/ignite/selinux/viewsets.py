8e7bf564560630a7e2c7c81b08a0bfd984965afa
jakob holst <jakob.holst@knowit.dk>
2024-01-31 14:20:46 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   pyproject.toml
    	modified:   src/ign8/ui/project/ignite/ignite/urls.py
    	modified:   src/ign8/ui/project/ignite/selinux/serializers.py
    	modified:   src/ign8/ui/project/ignite/selinux/urls.py
    	modified:   src/ign8/ui/project/ignite/selinux/viewsets.py
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- src/ign8/ui/project/ignite/selinux/viewsets.py
+++ src/ign8/ui/project/ignite/selinux/viewsets.py
@@ -3,8 +3,6 @@ from .models import Selinux
 from .serializers import SelinuxSerializer
 from django.views.decorators.csrf import csrf_exempt
 
-
-
 @csrf_exempt
 class selinuxAPIview(viewsets.ModelViewSet):
     queryset = Selinux.objects.all()
