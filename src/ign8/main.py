a74976d63e0c00cd64b1f00a9ff2c13acc2471f0
Jakob Holst <jakob.holst@knowit.dk>
2024-04-11 14:34:34 +0200

    dedk

--- src/ign8/main.py
+++ src/ign8/main.py
@@ -4,7 +4,93 @@ import subprocess
 import time
 import sys
 import redis
+import pprint
 import json
+import CloudFlare
+import socket
+
+
+def is_port_open(hostname, port):
+  """
+  This function checks if a remote host is listening on a specific port.
+
+  Args:
+      hostname: The hostname or IP address of the remote host.
+      port: The port number to check.
+
+  Returns:
+      True if the port is open, False otherwise.
+  """
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    try:
+      s.settimeout(1)  # Set a timeout of 1 second
+      s.connect((hostname, port))
+      return True
+    except socket.error:
+      pass
+  return False
+
+
+def get_swarm_status():
+    command = "docker info -f json |jq .Swarm.LocalNodeState -r"
+    result = subprocess.run(command, capture_output=True, shell=True)
+    if result.returncode == 0:
+        return result.stdout.decode().strip()
+    else:
+        return False
+
+def check_docker():
+    command = "docker info"
+    result = subprocess.run(command, capture_output=True, shell=True)
+    if result.returncode == 0:
+        prettylog("INFO", "Docker is running")
+        return True
+    else:
+        prettylog("ERROR", "Docker is not running")
+        return False
+    
+def check_remote_host(host):
+    print("-------------------------------------" + host)
+    port2377 = is_port_open(host, 2377)
+
+    if port2377:
+        prettylog("INFO", f"Port 2377 is open on {host}")
+        return True
+    else:
+        prettylog("ERROR", f"Port 2377 is closed on {host}")
+        return False
+    
+    
+
+
+    
+
+
+
+def read_dns_txt_record(domain):
+    command = f"dig {domain} TXT"
+    prettylog("INFO", f"Reading DNS TXT record for {domain}")
+    result = subprocess.run(command, capture_output=True, shell=True)
+    if result.returncode == 0:
+        output = result.stdout.decode()
+        start_index = output.find('"')
+        end_index = output.rfind('"')
+        txt_record = output[start_index:end_index + 1]
+        txt_record_cleaned = txt_record.strip('"').replace('\\010', '').replace('\\', '')
+        pprint.pprint(txt_record_cleaned)
+
+        txt_dict = json.loads(txt_record_cleaned)
+        return txt_dict
+    else:
+        prettylog("ERROR", f"Error reading DNS TXT record for {domain}")
+        return False
+    
+
+def cloudflare():
+    cf = CloudFlare.CloudFlare()
+    zones = cf.zones.get()
+    return cf
+
 
 def getenv(key, default):
     if key in os.environ:
@@ -13,15 +99,12 @@ def getenv(key, default):
         return default
 
 def main():
-    prettylog("INFO", )
     return True
 
 def in_venv():
     return sys.prefix != sys.base_prefix
 
 def init_service():
-    prettylog("INFO", "Init service")
-
     servicefile = "\
 [Unit]\n\
 Description=Ign8 Service\n\
@@ -29,7 +112,7 @@ After=network.target\n\
 \n\
 [Service]\n\
 Type=simple\n\
-User=ign8\n\
+User=root\n\
 ExecStart=/usr/local/bin/ign8 serve\n\
 Restart=always\n\
 \n\
@@ -42,19 +125,21 @@ WantedBy=multi-user.target\n\
     subprocess.run(["systemctl", "enable", "ign8"])
 
 def check_redis():
-    prettylog("INFO", "Check redis")
     try:
         r = redis.Redis(host='localhost', port=6379, db=0)
         r.ping()
     except redis.ConnectionError:
-        prettylog("ERROR", "Redis is not running")
-        prettylog("INFO", "Starting Redis")
         subprocess.run(["systemctl", "start", "redis"])
-    else:
-        prettylog("INFO", "Redis is running")
+    prettylog("INFO", "Redis is running")
 
     
-    
+def host_is_up(peer):
+    response = os.system("ping -c 1 " + peer)
+    if response == 0:
+        return True
+    else:
+        return False
+
 
 
 
@@ -67,8 +152,6 @@ def create_config_file():
     # create the directory
     if not os.path.exists('/etc/ign8'):
         os.makedirs('/etc/ign8')
-
-    prettylog("INFO", "Create config file")
     configfile = "\
 {\n\
     \"ign8\": {\n\
@@ -83,27 +166,11 @@ def create_config_file():
 def read_config():
     #check if the file exists
     if not os.path.exists('/etc/ign8/config.json'):
-        prettylog("INFO", "Config file does not exist")
-        if create_config_file():
-            prettylog("INFO", "Config file created")
-        else:
-            prettylog("ERROR", "Config file could not be created")
+        if not create_config_file():
             return False
-
-
-        
     open('/etc/ign8/config.json', 'r').read()
     myconfig = json.loads(open('/etc/ign8/config.json', 'r').read())
-    prettylog("INFO", "Read config", myconfig)
-    print(len(myconfig))
     return myconfig
-
-
-
-
-
-
-
 
 def start_service():
     prettylog("INFO", "start service")
@@ -111,21 +178,126 @@ def start_service():
 
 
 def stop_service():
-    prettylog("INFO", "stop service")
     subprocess.run(["systemctl", "stop", "ign8"])
+
+def cluster(myconfig):
+    cstat = {}
+
+    try: 
+        myorg = myconfig['organisation']
+    except:
+        myorg = "ajax"
+        prettylog("INFO", "No organisation found")
+    try: 
+        mycluster = myconfig['cluster']
+    except:
+        mycluster = "default"
+        prettylog("INFO", "No cluster found")
+    try:
+        myservices= myconfig['core_services']
+    except:
+        prettylog("INFO", "No core services found")
+
+    # is the cluster up on my local machine
+    if get_swarm_status() == "active":
+        cstat["local"] = "up"
+    else:
+        cstat["local"] = "down"
+        cstat["swarm"] = False
+
+
+    for clusterserver in mycluster:
+        cstat[clusterserver] = {}
+        
+        if host_is_up(clusterserver):
+            cstat[clusterserver]['ip'] = "up"
+            swarmdetect = check_remote_host(clusterserver)
+            if swarmdetect:
+                cstat[clusterserver]['swarm'] = "up"
+                cstat["swarm"] = True
+            else:
+                cstat[clusterserver]['swarm'] = "down"
+        else:
+            cstat[clusterserver]['ip'] = "down"
+    print("=============================================    ")
+    pprint.pprint(cstat)
+    if not cstat["swarm"] and cstat["local"] == "down":
+        prettylog("INFO", "No swarm cluster is up")
+        # initiate a new swarm cluster
+        prettylog("INFO", "Initiating a new swarm cluster")
+        command = "docker swarm init"
+        result = subprocess.run(command, capture_output=True, shell=True)
+        if result.returncode == 0:
+            prettylog("INFO", "Swarm cluster initiated")
+        else:
+            prettylog("ERROR", "Swarm cluster initiation failed")
+    if cstat["local"] == "up":
+        prettylog("INFO", "Swarm cluster is up")
+
+        command = "docker swarm join-token manager"
+        result = subprocess.run(command, capture_output=True, shell=True)
+        if result.returncode == 0:
+           pprint.pprint(  result.stdout.decode())
+
+
+        # check if the cluster is up
+        # check if the services are up
+        
+
+    print("=============================================    ")
+
+
+        
+
+
+    for service in myservices:
+        prettylog("INFO", "Service", service)
+        prettylog("INFO", "Sleeping")
+    prettylog("INFO", "Config file found")
+    prettylog("INFO", "Sleeping")
+    time.sleep(3)
+
 
 def serve():
     init_service()
     while True:
-        prettylog("info" , "Start loop")
-        time.sleep(1)
         check_redis()
+        myconfig = read_dns_txt_record("core.ign8.it")
+        prettylog("INFO", "Reading DNS TXT record for ign8.it")
+        cluster(myconfig)
+
+
+
+def therest():
         myconfig = read_config()
-        if not myconfig:
-            prettylog("ERROR", "Config file not found")
-            break
+        try: 
+            myorg = myconfig['organisation']
+        except:
+            myorg = "ajax"
+            prettylog("INFO", "No organisation found")
+        try: 
+            mycluster = myconfig['cluster']
+        except:
+            mycluster = "default"
+            prettylog("INFO", "No cluster found")
+        try:
+            myservices= myconfig['core_services']
+        except:
+            prettylog("INFO", "No core services found")
+
+        for clusterserver in mycluster:
+            prettylog("INFO", "Cluster server", clusterserver)
+            if host_is_up(clusterserver):
+                prettylog("INFO", "Host is up     : %s" % clusterserver)
+            else:
+                prettylog("ERROR", "Host is down  : %s" % clusterserver)
+        for service in myservices:
+            prettylog("INFO", "Service", service)
+            prettylog("INFO", "Sleeping")
         prettylog("INFO", "Config file found")
         prettylog("INFO", "Sleeping")
-        time.sleep(5)
-        
+        time.sleep(3)
+
+
+
 
