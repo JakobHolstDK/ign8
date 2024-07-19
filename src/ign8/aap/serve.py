171a5ec40f226b3d71a0877302498abcab9ee5be
jakob Holst <lyngknuden@gmail.com>
2024-07-19 08:24:10 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   pyproject.toml
    	modified:   src/ign8/aap/serve.py
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- src/ign8/aap/serve.py
+++ src/ign8/aap/serve.py
@@ -101,6 +101,8 @@ def login_aap_basicauth(url, user, password):
   print("Login successful")
   # we need to create a token
   tokenurl = url + "/api/v2/tokens"
+  pprint.pprint(data)
+  
   resp = requests.post(tokenurl, headers=headers, data=json.dumps(data), verify=False)
   pprint.pprint(resp.content)
 
