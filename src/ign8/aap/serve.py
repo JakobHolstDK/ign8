83e9acd95719f783ffea27659bfcdf4f48af2c8f
jakob Holst <lyngknuden@gmail.com>
2024-07-19 07:26:56 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   pyproject.toml
    	modified:   src/ign8/aap/serve.py
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- src/ign8/aap/serve.py
+++ src/ign8/aap/serve.py
@@ -27,10 +27,15 @@ def get_credentials_from_vault():
     read_response = client.secrets.kv.read_secret_version(path=vaultpath)
   except:
     # If the path does not exist, create it
+    # read the aap user, url and password from vault , obscufate the password input
+    # create the path if it does not exist
+    AAP_URL = input("Enter the AAP URL: ")
+    AAP_USER = input("Enter the AAP USER: ")
+    AAP_PASS = input("Enter the AAP PASSWORD: ") # obscufate the password input
     client.secrets.kv.v2.create_or_update_secret(
-    path=vaultpath,
-    secret=dict(AAP_URL='awxrpm.openknowoit.com', AAP_USER='admin', AAP_PASS='admin'),
-)
+      path=vaultpath,
+      secret=dict(AAP_URL=AAP_URL, AAP_USER=AAP_USER, AAP_PASS=AAP_PASS),
+    )
   read_response = client.secrets.kv.read_secret_version(path=vaultpath)
   return read_response['data']['data']
 
