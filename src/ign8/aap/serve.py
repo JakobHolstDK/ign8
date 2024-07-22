9f7395d4502c03686aaed6eea703117138c74cba
jakob Holst <lyngknuden@gmail.com>
2024-07-22 08:10:26 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   pyproject.toml
    	modified:   src/ign8/aap/serve.py
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    	src/ign8/aap/bitbucket.py
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- src/ign8/aap/serve.py
+++ src/ign8/aap/serve.py
@@ -317,7 +317,7 @@ def main():
       bbtoken = get_bitbucket_token("ignite/bitbucket")
       projects = get_bitbucket_project_list(bbtoken)
       # Check if må main project exists i bitbucket
-      pprint.pprint( config['mainproject']['mainproject']['name'])
+      pprint.pprint( config['mainproject']['mainproject'])
       project = get_bitbucket_project(bbtoken, config['mainproject']['mainproject']['name'])
       if project == False:
         prettyllog("Ignite aap", "Main loop", "Refresh AWX data", "automation platform", "0", "Main project does not exist", "ERROR")
